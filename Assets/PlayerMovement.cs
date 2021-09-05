094d16853413fc298f99f16cfe52f73c26fc561b
PTRL292\Bhuvaneswari Gedela <bhuvaneswari.gedela@xcubelabs.com>
2021-09-05 17:31:01 +0530

    assets and player movement are added

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController characterController;
+    public float playerSpeed;
+    public float backSpeed;
+    public float turnSpeed;
+    Animator anim;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        anim = GetComponentInChildren<Animator>();
+      
+    }
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+        var movement = new Vector3(horizontal, 0, vertical);
+        anim.SetFloat("Speed", vertical);
+        transform.Rotate(Vector3.up, horizontal * turnSpeed * Time.deltaTime);
+        if (vertical != 0)
+        {
+            float moveSpeed = (vertical > 0) ? playerSpeed : backSpeed;
+            characterController.SimpleMove(transform.forward * vertical * moveSpeed);
+           
+        }
+
+    }
+   
+}
