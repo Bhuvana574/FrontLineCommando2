969ac92419fa05be6027518e16eed3005e83fa29
PTRL292\Bhuvaneswari Gedela <bhuvaneswari.gedela@xcubelabs.com>
2021-09-08 14:08:41 +0530

    scripts are added

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    AggroDetection aggro;
+    NavMeshAgent nav;
+    Transform enemyTarget;
+    //public Transform player;
+    public static EnemyMovement instance;
+    public float enemySpeed;
+    Animator anim;
+    private void Awake()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
+        aggro = GetComponent<AggroDetection>();
+        aggro.OnAggro += Aggro_OnAggro;
+        instance = this;
+    }
+    private void Aggro_OnAggro(Transform target)
+    {
+        this.enemyTarget = target;
+    }
+    private void Update()
+    {
+        if (enemyTarget != null)
+        {
+            nav.SetDestination(enemyTarget.position);
+            //transform.LookAt(player);
+            enemySpeed = nav.velocity.magnitude;
+            anim.SetFloat("Speed", enemySpeed);
+        }
+    }
+}
